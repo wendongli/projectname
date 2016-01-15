b4601c87a2a0dc900cd73b0b890ecb5fad3d8f27
wendong.lwd <wendong.lwd@alibaba-inc.com>
2016-01-15 14:06:30 +0800

    删除注释

--- dal/src/main/java/com/lwd/test/projectname/dal/dao/CpUserDao.java
+++ dal/src/main/java/com/lwd/test/projectname/dal/dao/CpUserDao.java
@@ -1,18 +1,9 @@
-/*
- * Copyright 2014 Alibaba.com All right reserved. This software is the
- * confidential and proprietary information of Alibaba.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Alibaba.com.
- */
 package com.lwd.test.projectname.dal.dao;
 
 import com.lwd.test.projectname.dal.pojo.CpUserPojo;
 
 /**
  * CP用户Dao
- * 
- * @author wendong.lwd
  * 
  */
 public interface CpUserDao {
